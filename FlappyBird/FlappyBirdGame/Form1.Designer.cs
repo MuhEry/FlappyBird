bc7a202c293298ed7d2ea0b2cc59f7718622f318
Muhammed Eryılmaz <muherylmaz@gmail.com>
2024-10-26 17:26:41 +0300

    FlappyBirdGame
    C# form ile hazırlandı.

---
+++ FlappyBird/FlappyBirdGame/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FlappyBirdGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.skor = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.zemin = new System.Windows.Forms.PictureBox();
+            this.boruAlt = new System.Windows.Forms.PictureBox();
+            this.boruUst = new System.Windows.Forms.PictureBox();
+            this.kus = new System.Windows.Forms.PictureBox();
+            this.menu = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boruAlt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boruUst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // skor
+            // 
+            this.skor.AutoSize = true;
+            this.skor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.skor.Font = new System.Drawing.Font("Swis721 BlkCn BT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.skor.Location = new System.Drawing.Point(12, 9);
+            this.skor.Name = "skor";
+            this.skor.Size = new System.Drawing.Size(80, 36);
+            this.skor.TabIndex = 0;
+            this.skor.Text = "Skor:";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.gameTimer);
+            // 
+            // zemin
+            // 
+            this.zemin.Location = new System.Drawing.Point(-1, 557);
+            this.zemin.Name = "zemin";
+            this.zemin.Size = new System.Drawing.Size(651, 240);
+            this.zemin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.zemin.TabIndex = 2;
+            this.zemin.TabStop = false;
+            // 
+            // boruAlt
+            // 
+            this.boruAlt.Location = new System.Drawing.Point(500, 360);
+            this.boruAlt.Name = "boruAlt";
+            this.boruAlt.Size = new System.Drawing.Size(100, 325);
+            this.boruAlt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.boruAlt.TabIndex = 4;
+            this.boruAlt.TabStop = false;
+            // 
+            // boruUst
+            // 
+            this.boruUst.Location = new System.Drawing.Point(500, -100);
+            this.boruUst.Name = "boruUst";
+            this.boruUst.Size = new System.Drawing.Size(100, 272);
+            this.boruUst.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.boruUst.TabIndex = 3;
+            this.boruUst.TabStop = false;
+            // 
+            // kus
+            // 
+            this.kus.Location = new System.Drawing.Point(66, 199);
+            this.kus.Name = "kus";
+            this.kus.Size = new System.Drawing.Size(65, 56);
+            this.kus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kus.TabIndex = 1;
+            this.kus.TabStop = false;
+            // 
+            // menu
+            // 
+            this.menu.AutoSize = true;
+            this.menu.Font = new System.Drawing.Font("Agency FB", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menu.Location = new System.Drawing.Point(82, 413);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(352, 44);
+            this.menu.TabIndex = 5;
+            this.menu.Text = "Oynamak için SPACE basınız";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(541, 761);
+            this.Controls.Add(this.menu);
+            this.Controls.Add(this.skor);
+            this.Controls.Add(this.kus);
+            this.Controls.Add(this.boruUst);
+            this.Controls.Add(this.zemin);
+            this.Controls.Add(this.boruAlt);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.asagı);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.yukari);
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boruAlt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boruUst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kus)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label skor;
+        private System.Windows.Forms.PictureBox kus;
+        private System.Windows.Forms.PictureBox zemin;
+        private System.Windows.Forms.PictureBox boruUst;
+        private System.Windows.Forms.PictureBox boruAlt;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label menu;
+    }
+}
+
---
+++ FlappyBird/FlappyBirdGame/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FlappyBirdGame
+{
+    public partial class Form1 : Form
+    {
+        Random rastgele = new Random();
+        int boruHızı = 9;  
+        int yercekimi = 11;
+        int puan = 0;
+        bool oyunBasladi = false;
+        int rekor = 0;
+        public Form1()
+        {
+            InitializeComponent();  // Form üzerindeki bileşenleri başlatır
+            OyunuBaslat();  // Oyunu başlatmak için gerekli ayarları yapar
+            menu.Visible = true;  // Başlangıç menüsü ekranda görünür olur
+        }
+        private void OyunuBaslat()
+        {
+            // Oyun başlatılmadan önce borular ve kuşun başlangıç pozisyonu ile puan ayarlanır
+            kus.Top = this.Height / 2 - 80; 
+            boruAlt.Left = 370;
+            boruUst.Left = 370;
+            boruAlt.Top = 300;
+            boruUst.Top = -100;
+            puan = 0;  
+            skor.Text = "Skor: " + puan;  
+            oyunBasladi = false;  // Oyun başlangıçta durdurulmuş
+            timer.Stop();  // Oyun başlamadan önce zamanlayıcı durdurulmalı
+            menu.Visible = true;  // Başlangıç yazısını görünür yap
+        }
+
+        private void gameTimer(object sender, EventArgs e)  // Zamanlayıcı ile sürekli olarak çağrılan oyun döngüsü
+        {
+            if (!oyunBasladi) return;  // Eğer oyun başlamadıysa, timer'ın geri kalanı çalışmaz
+
+            kus.Top += yercekimi;  // Kuşa yerçekimi etkisi uygula
+
+            boruAlt.Left -= boruHızı;  // Boruları sola hareket ettir
+            boruUst.Left -= boruHızı;
+
+            skor.Text = "Skor: " + puan;  // Skoru güncel tut
+
+            if (0 < boruAlt.Left && boruAlt.Left < 10)  // Borular kuşu geçince puanı artır
+            {
+                puan++;
+            }
+            if(boruAlt.Left < -100)  // Borular ekranın solundan çıktıysa konumlarını tekrar ayarla ve yeni rastgele yükseklik ayarla
+            {
+                int konum = rastgele.Next(-90,90);  // rastgele yükseklik üret
+                boruAlt.Left = 400;
+                boruUst.Left = 400;
+                boruAlt.Top = 300 + konum;
+                boruUst.Top = -100 + konum;
+            }            
+
+            if(kus.Bounds.IntersectsWith(boruAlt.Bounds) || kus.Bounds.IntersectsWith(boruUst.Bounds) ||
+            kus.Bounds.IntersectsWith(zemin.Bounds) || kus.Bounds.Top < -25)  // Kuş bir engelle veya zeminle çarpışırsa oyunu bitir
+            {
+                OyunuBitir();
+            }
+        }
+        public void OyunuBitir()
+        {
+            timer.Stop();  // zamanlayıcı durduruldu
+
+            if (puan > rekor)  // rekor geçildiyse güncelle
+            {
+                rekor = puan;
+            }
+            MessageBox.Show("OYUN BİTTİ  SKOR: " + puan + " REKOR: " + rekor + "\nTekrar oynamak için Tamam'a basın");
+            // Oyun bitiş mesajını göster
+            OyunuBaslat(); 
+        }
+
+        private void asagı(object sender, KeyEventArgs e)  // Space tuşuna basıldığında çalışır
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                if (!oyunBasladi)
+                {
+                    oyunBasladi = true;  // Eğer oyun başlamamışsa başlat
+                    yercekimi = 11;  // Oyun başladıktan sonra yer çekimi aktif olsun
+                    timer.Start();  // Zamanlayıcı başlatılsın
+                    menu.Visible = false;  // Menü gizlensin
+                }
+                yercekimi = -11;  // Space tuşuna basıldığında kuşu yukarı doğru hareket ettir
+            }
+        }
+
+        private void yukari(object sender, KeyEventArgs e)  // Space tuşuna bırakıldığında çalışır
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                yercekimi = 11;  // Space tuşu bırakıldığında kuş tekrar aşağı yönlü hareket etsin
+            }
+        }
+    }
+}
